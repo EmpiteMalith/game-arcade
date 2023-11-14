f2bc8292ef2ba1bbb37159f7bb26fa3dbb2550bb
empitemalith <malith@empite.com>
2023-11-14 21:35:19 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-normal text-red-400">
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe veniam
+        nesciunt vitae iure nisi labore odit debitis in molestias, deserunt
+        aliquid nobis minima enim mollitia quisquam? Quis veniam deserunt
+        assumenda.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
