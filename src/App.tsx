fe1c1f3898c6ee6d906622d61dd890aabf4b9aee
empitemalith <malith@empite.com>
2023-11-22 21:22:20 +0530

    layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,104 @@
-
 import "./App.css";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="dark:bg-darkBlack h-screen">
-      <NavBar/>
+    <div className="dark:bg-darkBlack flex flex-col">
+      <NavBar />
+      <div className="w-full flex gap-3 h-full p-5">
+        <div className="flex-1 bg-gray-300 h-full"><p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p></div>
+        <div className="flex-4 bg-gray-500 h-full">
+          <p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p>
+          <p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p>
+          <p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p>
+          <p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p>
+          <p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p>
+          <p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p>
+          <p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p><p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p>
+          <p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p>
+          <p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p><p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p><p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p><p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p><p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p><p className="text-lg mt-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Praesentium incidunt aut consectetur. Sit eligendi illum enim
+            consectetur? Ea qui quos corrupti velit nostrum temporibus omnis ab
+            voluptatum, doloremque voluptate architecto.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
